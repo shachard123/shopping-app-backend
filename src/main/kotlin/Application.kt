4eebeddf23ac25e42f98ba058e6e006da06af617
shachard123 <shachardahan10@gmail.com>
2025-02-25 10:33:19 +0200

    INIT

---
+++ src/main/kotlin/Application.kt
@@ -0,0 +1,14 @@
+package com.shopping
+
+import io.ktor.server.application.*
+
+fun main(args: Array<String>) {
+    io.ktor.server.netty.EngineMain.main(args)
+}
+
+fun Application.module() {
+    configureSerialization()
+    configureDatabases()
+    configureSecurity()
+    configureRouting()
+}
---
+++ src/main/kotlin/Serialization.kt
@@ -0,0 +1,23 @@
+package com.shopping
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import com.mongodb.client.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import io.ktor.server.config.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureSerialization() {
+    routing {
+        get("/json/kotlinx-serialization") {
+                call.respond(mapOf("hello" to "world"))
+            }
+    }
+}
